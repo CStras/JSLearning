dda4d612af35a1241908dd91f3bb730fa8bf2b2d
CStras <157181707+CStras@users.noreply.github.com>
2024-06-05 17:08:13 -0500

    PE 1.4 added

---
+++ practice-exercises/PE-1.4.js
@@ -0,0 +1,8 @@
+// exercise for commenting and semicolon use
+
+let a = 10; // set value of a to 10
+
+console.log(a) // will print a to console log and will display value of a
+
+/* As another example, this is a multi-line comment using an * in between two //
+*/
