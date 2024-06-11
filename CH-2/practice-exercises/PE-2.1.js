b732e72517565c89dbc387934000f5e26d150af2
CStras <157181707+CStras@users.noreply.github.com>
2024-06-11 16:07:12 -0500

    fix: added exercise 2.1

---
+++ CH-2/practice-exercises/PE-2.1.js
@@ -0,0 +1,11 @@
+let str1 = 'Laurence'; //string
+let str2 = "Svekis"; //string
+let val1 = undefined; //undefined
+let val2 = null; //null
+let myNum = 1000; //number or integer
+
+console.log(typeof (str1));
+console.log(typeof (str2));
+console.log(typeof (val1));
+console.log(typeof (val2));
+console.log(typeof (myNum));
