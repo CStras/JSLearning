85f7f907210d0ffd3d96d050b6733304201fdc0c
CStras <157181707+CStras@users.noreply.github.com>
2024-06-11 16:22:29 -0500

    fix: spelling

--- CH-2/practice-exercises/PE-2.1.js
+++ CH-2/practice-exercises/PE-2.1.js
@@ -1,8 +1,8 @@
 let str1 = 'Laurence'; //string
 let str2 = "Svekis"; //string
 let val1 = undefined; //undefined
-let val2 = null; //null
-let myNum = 1000; //number or integer
+let val2 = null; //object
+let myNum = 1000; //number
 
 console.log(typeof (str1));
 console.log(typeof (str2));
