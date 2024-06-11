4bf482794791cb9631b066ab35e3d9dad50bdd4c
CStras <157181707+CStras@users.noreply.github.com>
2024-06-11 17:34:10 -0500

    fix: added alert

--- CH-2/CH-2 project/mToKm.js
+++ CH-2/CH-2 project/mToKm.js
@@ -4,3 +4,4 @@ let conv = 1.60934;
 let km = miles * conv;
 
 console.log("The distance of " + miles + "is equal to " + km + "kms");
+alert("The distance of " + miles + "is equal to " + km + "kms");
